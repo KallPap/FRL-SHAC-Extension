f3702d7ec48aa45b5e32498b20fcc11cd149fa97
KallPap <91315860+KallPap@users.noreply.github.com>
2024-05-26 20:19:28 +0200

    fix

--- models/actor_checkpoint_gd.py
+++ models/actor_checkpoint_gd.py
@@ -45,20 +45,15 @@ class ActorDeterministicMLP(nn.Module):
     def forward(self, observations, deterministic = False):
         return self.actor(observations)
 
-
 from torch.utils.checkpoint import checkpoint
 
 class ActorStochasticMLP(nn.Module):
     def __init__(self, obs_dim, action_dim, cfg_network, device='cuda:0'):
         super(ActorStochasticMLP, self).__init__()
-
         self.device = device
-
         self.layer_dims = [obs_dim] + cfg_network['actor_mlp']['units'] + [action_dim]
+        init_ = lambda m: model_utils.init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), np.sqrt(2))
 
-        init_ = lambda m: model_utils.init(m, nn.init.orthogonal_, lambda x: nn.init.
-                        constant_(x, 0), np.sqrt(2))
-        
         modules = []
         for i in range(len(self.layer_dims) - 1):
             modules.append(nn.Linear(self.layer_dims[i], self.layer_dims[i + 1]))
@@ -67,57 +62,48 @@ class ActorStochasticMLP(nn.Module):
                 modules.append(torch.nn.LayerNorm(self.layer_dims[i+1]))
             else:
                 modules.append(model_utils.get_activation_func('identity'))
-            
+
         self.mu_net = nn.Sequential(*modules).to(device)
-
         logstd = cfg_network.get('actor_logstd_init', -1.0)
-
         self.logstd = torch.nn.Parameter(torch.ones(action_dim, dtype=torch.float32, device=device) * logstd)
-
         self.action_dim = action_dim
         self.obs_dim = obs_dim
-
         print(self.mu_net)
         print(self.logstd)
-    
+
     def get_logstd(self):
         return self.logstd
 
-    def forward(self, obs, deterministic = False):
+    def forward(self, obs, deterministic=False):
+        obs.requires_grad_(True)
         def custom_forward(*inputs):
             return self.mu_net(inputs[0])
-        
-        mu = checkpoint(custom_forward, obs)
-
+        mu = checkpoint(custom_forward, obs, use_reentrant=True)
         if deterministic:
             return mu
         else:
-            std = self.logstd.exp() # (num_actions)
+            std = self.logstd.exp()
             dist = Normal(mu, std)
             sample = dist.rsample()
             return sample
-    
-    def forward_with_dist(self, obs, deterministic = False):
+
+    def forward_with_dist(self, obs, deterministic=False):
+        obs.requires_grad_(True)
         def custom_forward(*inputs):
             return self.mu_net(inputs[0])
-        
-        mu = checkpoint(custom_forward, obs)
-        std = self.logstd.exp() # (num_actions)
-
+        mu = checkpoint(custom_forward, obs, use_reentrant=True)
+        std = self.logstd.exp()
         if deterministic:
             return mu, mu, std
         else:
             dist = Normal(mu, std)
             sample = dist.rsample()
             return sample, mu, std
-        
     def evaluate_actions_log_probs(self, obs, actions):
+        obs.requires_grad_(True)
         def custom_forward(*inputs):
             return self.mu_net(inputs[0])
-        
-        mu = checkpoint(custom_forward, obs)
-
+        mu = checkpoint(custom_forward, obs, use_reentrant=True)
         std = self.logstd.exp()
         dist = Normal(mu, std)
-
         return dist.log_prob(actions)
