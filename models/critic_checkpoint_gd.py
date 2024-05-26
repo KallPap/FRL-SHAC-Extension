8cff789e2df8aca74db3241aa5e336d9f602a7f9
KallPap <91315860+KallPap@users.noreply.github.com>
2024-05-26 20:21:10 +0200

    fix critic

--- models/critic_checkpoint_gd.py
+++ models/critic_checkpoint_gd.py
@@ -5,40 +5,30 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
+from torch.utils.checkpoint import checkpoint
 import torch
 import torch.nn as nn
-import numpy as np
-
 from models import model_utils
-
-from torch.utils.checkpoint import checkpoint
 
 class CriticMLP(nn.Module):
     def __init__(self, obs_dim, cfg_network, device='cuda:0'):
         super(CriticMLP, self).__init__()
-
         self.device = device
-
         self.layer_dims = [obs_dim] + cfg_network['critic_mlp']['units'] + [1]
-
-        init_ = lambda m: model_utils.init(m, nn.init.orthogonal_, lambda x: nn.init.
-                        constant_(x, 0), np.sqrt(2))
-                        
+        init_ = lambda m: model_utils.init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), np.sqrt(2))
         modules = []
         for i in range(len(self.layer_dims) - 1):
             modules.append(init_(nn.Linear(self.layer_dims[i], self.layer_dims[i + 1])))
             if i < len(self.layer_dims) - 2:
                 modules.append(model_utils.get_activation_func(cfg_network['critic_mlp']['activation']))
                 modules.append(torch.nn.LayerNorm(self.layer_dims[i + 1]))
-
         self.critic = nn.Sequential(*modules).to(device)
-    
         self.obs_dim = obs_dim
-
         print(self.critic)
 
     def forward(self, observations):
+        observations.requires_grad_(True)
         def custom_forward(*inputs):
             return self.critic(inputs[0])
-        
-        return checkpoint(custom_forward, observations)
+        return checkpoint(custom_forward, observations, use_reentrant=True)
+
